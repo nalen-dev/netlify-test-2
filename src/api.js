a581184d10def859be1ace1a586e3d9a60c30e19
nlndrpraja <nalendrapraja11@gmail.com>
2023-01-01 02:31:50 +0800

    ada mongodb

--- src/api.js
+++ src/api.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const MongoClient = require("mongodb").MongoClient;
+
+const router = express.Router();
 
 const app = express();
+
+const url = "mongodb+srv://admin:admin123@todolist-cluster.kpe60gd.mongodb.net/todolist?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParse: true });
 
 router.use(express.json());
 
@@ -9,6 +15,12 @@ router.get("/", (req, res) => {
   res.json({
     hello: "hi!",
   });
+});
+
+router.get("/database", async (req, res) => {
+  const collection = client.db("todolist").collection("users");
+  const data = await collection.findOne();
+  return res.json(data);
 });
 
 app.use(`/.netlify/functions/api`, router);
